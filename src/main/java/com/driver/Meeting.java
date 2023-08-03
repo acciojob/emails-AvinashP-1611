e51a52422510c83b8b8aa541ee5b589fb61eb06d
Avinash Prajapati <avinashprajapati1611r@gmail.com>
2023-08-03 14:06:49 +0530

    c

--- src/main/java/com/driver/Meeting.java
+++ src/main/java/com/driver/Meeting.java
@@ -3,18 +3,20 @@ package com.driver;
 import java.time.LocalTime;
 
 public class Meeting {
-    LocalTime startTime;
-    LocalTime endTime;
+    private LocalTime startTime;
+    private LocalTime endTime;
+
 
     public Meeting(LocalTime startTime, LocalTime endTime){
-        this.startTime = startTime;
-        this.endTime = endTime;
-    }
-    public LocalTime getStartTime() {
-        return startTime;
-    }
+            this.startTime = startTime;
+            this.endTime = endTime;
+        }
 
-    public LocalTime getEndTime() {
-        return endTime;
-    }
+        public LocalTime getStartTime() {
+            return startTime;
+        }
+
+        public LocalTime getEndTime() {
+            return endTime;
+        }
 }
